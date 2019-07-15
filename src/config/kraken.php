2e5b7bb50edfea03334dc625b2ce3358a4114303
Victor Caceres <caceres.vc95@gmail.com>
2019-07-15 13:50:32 -0400

    changes

--- src/config/kraken.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-	'kraken_host' 	=> 	env('KRAKEN_HOST'),
-	'kraken_version'=>	env('KRAKEN_VERSION'),
-	'kraken_key'	=>	env('KRAKEN_KEY'),
-	'kraken_secret'	=>	env('KRAKEN_SECRET')
-];
