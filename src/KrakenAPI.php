b535aabb5551740086bc133c7c30ad7cf6c7d81c
Victor Caceres <caceres.vc95@gmail.com>
2019-07-15 13:53:33 -0400

    changes

--- src/KrakenAPI.php
+++ src/KrakenAPI.php
@@ -332,6 +332,7 @@ class KrakenAPI
     {
         // build the POST data string
         $postdata = http_build_query($request, '', '&');
+
         // make request
         curl_setopt($this->curl, CURLOPT_URL, $this->url . '/' . $this->version . '/public/' . $method);
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, $postdata);
